6cb63f867003a53f78ea1652dc967d05908f410e
Jayendra2410 <jayendrasinghjadone@gmail.com>
2025-12-18 17:35:07 +0530

    Singly Linked List

---
+++ LinkedListSingly.java
@@ -0,0 +1,28 @@
+public class LinkedListSingly {
+    private ListNode head;
+    private static class ListNode {
+        private int data;
+        private ListNode next;
+        ListNode(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public void display(){
+        ListNode current = head;
+        while(current != null){
+            System.err.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String[] args) {
+        LinkedListSingly list = new LinkedListSingly();
+        list.head = new ListNode(10);
+        ListNode second = new ListNode(20);
+        ListNode third = new ListNode(30);
+        list.head.next = second;
+        second.next = third;
+        list.display();
+    }
+}
